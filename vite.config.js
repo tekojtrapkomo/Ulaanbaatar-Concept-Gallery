132084f5bc419b024d15118c8f36ccff97829075
tekojtrapkomo <nyamdavaabattogtoh@gmail.com>
2023-04-06 01:34:26 +0800

    font faster revert

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-function VitePluginRemoveHashFontFiles() {
-	return {
-		name: 'Remove hash from font files',
-		apply: 'build',
-		enforce: 'post',
-		config(config) {
-			const defaultAssetFileNames = config.build.rollupOptions.output.assetFileNames;
-			config.build.rollupOptions.output.assetFileNames = ({ type, name }) => {
-				if (type === 'asset' && /\.(woff2?|ttf|otf)$/.test(name)) return `[name].[ext]`;
-				return defaultAssetFileNames;
-			};
-		}
-	};
-}
+
 export default defineConfig({
-	plugins: [sveltekit(), VitePluginRenameCSSFontFiles()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
